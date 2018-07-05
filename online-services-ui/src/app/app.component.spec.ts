b95fd7bf69a720a32ba99cebbfec9c61ef573b81
ivmr <ivan.mrva@gmail.com>
2018-07-05 14:22:08 +0200

    fixed karma unit test

--- online-services-ui/src/app/app.component.spec.ts
+++ online-services-ui/src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
-import { async, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { ShowCaseRoutingModule } from './show-case/show-case-routing.module';
-import { ShowCaseModule } from './show-case/show-case.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
 
@@ -17,8 +15,6 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        ShowCaseRoutingModule,
-        ShowCaseModule,
         BrowserAnimationsModule],
 
       declarations: [AppComponent],
