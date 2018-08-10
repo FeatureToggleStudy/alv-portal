bac6b7a577107398d4351064c7e9af71b991fed4
ivanmrva <ivanmrva@users.noreply.github.com>
2018-08-10 23:51:03 +0100

    Task/ci script refactoring (#47)
    travis pipeline refactoring (extracting commands to separated shell scripts)

--- ci/utils/check-health.sh
+++ ci/utils/check-health.sh
@@ -2,9 +2,10 @@
 
 URL=$1
 
+echo "Downloading 'wait-for-it' script..."
 wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-
 chmod +x ./wait-for-it.sh
 
+echo "Checking application health..."
 ./wait-for-it.sh -t 60 $URL -- echo "Online Services App is UP!"
 
