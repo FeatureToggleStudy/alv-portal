8206be595c1ab94f4a9c3a1ec82dcb0d50e75ac9
Iurii Katkov <iurii.katkov@mimacom.com>
2018-09-14 14:50:43 +0200

    i18n for shared modules

--- online-services-ui/src/app/app.module.ts
+++ online-services-ui/src/app/app.module.ts
@@ -5,10 +5,21 @@ import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+import {  SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+/**
+ * Setting up the ngx-translate
+ * @param http
+ */
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, environment.translationBaseUrl, '.json');
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -17,6 +28,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     NgbTabsetModule,
     SharedModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
     FormsModule,
     ReactiveFormsModule,
     CoreModule
