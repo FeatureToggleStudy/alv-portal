e2e90a3f510aa15b660bdf0bf27d73a9f362a71f
Iurii Katkov <iurii.katkov@mimacom.com>
2018-10-03 15:43:16 +0200

    #DF-120 jobseeker landing page using existing alv widgets

--- alv-portal-ui/src/app/core/landing/job-seeker-dashboard-page/job-seeker-dashboard-page.component.ts
+++ alv-portal-ui/src/app/core/landing/job-seeker-dashboard-page/job-seeker-dashboard-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { Observable } from 'rxjs';
+import { User } from '../../authentication/user.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alv-job-seeker-dashboard-page',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobSeekerDashboardPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authenticationService: AuthenticationService,
+              private fb: FormBuilder) { }
+  currentUser$: Observable<User>;
+  findWorkerForm: FormGroup;
 
   ngOnInit() {
+    this.findWorkerForm = this.fb.group({
+      profession: this.fb.control('Designer'),
+      city: this.fb.control('Zurich')
+    });
+    this.currentUser$  =  this.authenticationService.getCurrentUser()
   }
 
 }
