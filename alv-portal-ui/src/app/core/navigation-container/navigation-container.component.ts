6f203799762eabda5576c2a6c4ed7b01359b69b8
Iurii Katkov <iurii.katkov@mimacom.com>
2018-10-04 13:01:32 +0200

    #DF-120 switching pictures by watching routing

--- alv-portal-ui/src/app/core/navigation-container/navigation-container.component.ts
+++ alv-portal-ui/src/app/core/navigation-container/navigation-container.component.ts
@@ -1,5 +1,11 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+const PATH_TO_PICTURE = {
+  '/home/job-seeker': "jobseeker-home",
+  '/home/company': "company-home",
+  '/home/pav': "pav-home"
+};
 @Component({
   selector: 'alv-navigation-container',
   templateUrl: './navigation-container.component.html',
@@ -8,10 +14,19 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 export class NavigationContainerComponent implements OnInit {
 
   @HostBinding('class') readonly class = 'd-block d-md-flex';
+  public currentMainStyle: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    // the component is watching for the changes of the router path and applies respective
+    // style to the main component. This is done for switching background images
+    this.router.events
+        .subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            this.currentMainStyle = PATH_TO_PICTURE[event.urlAfterRedirects];
+          }
+        });
   }
 
 }
