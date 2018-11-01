a2f1df1b45a07bb95294a7ba5c1292fd552c83f7
Sven Heinrich <sven.heinrich@mimacom.com>
2018-11-01 13:04:38 +0100

    Add animation for notifications, small bugfixes

--- alv-portal-ui/src/app/shared/layout/notifications/notifications/notifications.component.ts
+++ alv-portal-ui/src/app/shared/layout/notifications/notifications/notifications.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Notification } from '../notification.model';
 import { NotificationsService } from '../../../../core/notifications.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'alv-notifications-container',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
+  animations: [
+    trigger('insertRemoveNotification', [
+      transition(':enter', [
+        style({ transform: 'translateY(-200px)', opacity: 1 }),
+        animate('0.5s ease-in-out', style({ transform: 'translateY(0)', opacity: 1})),
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in', style({ transform: 'translateX(120%)', opacity: 0.5 }))
+      ])
+    ]),
+  ]
 })
 export class NotificationsComponent implements OnInit {
 
