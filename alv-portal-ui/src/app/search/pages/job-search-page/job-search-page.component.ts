c61510c13446edb104deabf873e6eda9da09830d
Iurii Katkov <iurii.katkov@mimacom.com>
2018-11-26 08:47:08 +0100

    infinite scroll

--- alv-portal-ui/src/app/search/pages/job-search-page/job-search-page.component.ts
+++ alv-portal-ui/src/app/search/pages/job-search-page/job-search-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JobAdvertisementService } from '../../services/job-advertisement/job-advertisement.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
 import {
   JobAdvertisementSearchRequest,
   JobAdvertisementSearchRequestBody
@@ -8,49 +8,60 @@ import {
 import { JobSearchFilter } from './job-search.model';
 import { JobSearchRequestMapperService } from './job-search-request-mapper.service';
 import { JobAdvertisement } from '../../services/job-advertisement/job-advertisement.model';
+import { map, switchAll, takeUntil } from 'rxjs/operators';
+import { AbstractSubscriber } from '../../../core/abstract-subscriber';
 
 export const ITEMS_PER_PAGE = 10;
+
 @Component({
   selector: 'alv-job-search-page',
   templateUrl: './job-search-page.component.html',
   styleUrls: ['./job-search-page.component.scss']
 })
-export class JobSearchPageComponent implements OnInit {
+export class JobSearchPageComponent extends AbstractSubscriber implements OnInit {
 
-  resultList$: Observable<JobAdvertisement[]>;
+  resultList: JobAdvertisement[] = [];
+  private scroll$: BehaviorSubject<number> = new BehaviorSubject(0);
+  private filtersChange$: Subject<JobSearchFilter> = new Subject();
 
   constructor(private jobAdsService: JobAdvertisementService,
               private mapper: JobSearchRequestMapperService) {
+    super();
   }
 
   ngOnInit() {
+    //fixme should be moved to a server
+    combineLatest(this.filtersChange$, this.scroll$).pipe(
+      map(([filtersValues, page]) => {
+        const body: JobAdvertisementSearchRequestBody = {
+          workloadPercentageMin: filtersValues.workloadPercentageMin,
+          workloadPercentageMax: filtersValues.workloadPercentageMax,
+          permanent: this.mapper.mapContractType(filtersValues.contractType),
+          companyName: filtersValues.company,
+          onlineSince: 50,
+          displayRestricted: false
+        };
+
+        const searchRequest: JobAdvertisementSearchRequest = {
+          page: page,
+          size: ITEMS_PER_PAGE,
+          sort: this.mapper.mapSort(filtersValues.sort),
+          body: body
+        };
+        return this.jobAdsService.search(searchRequest);
+      }),
+      switchAll(),
+      takeUntil(this.ngUnsubscribe))
+      .subscribe((resultsFromServer: JobAdvertisement[]) => {
+        this.resultList.push(...resultsFromServer);
+      })
   }
 
   onFiltersChange(filtersValues: JobSearchFilter, page = 0) {
-
-    //here we just want to trigger subject next and then do combineLatest with the scrolling
-
-    //we don't do any manipulations with the data that comes from the autocompletion yet
-    const body: JobAdvertisementSearchRequestBody = {
-      workloadPercentageMin: filtersValues.workloadPercentageMin,
-      workloadPercentageMax: filtersValues.workloadPercentageMax,
-      permanent: this.mapper.mapContractType(filtersValues.contractType),
-      companyName: filtersValues.company,
-      onlineSince: 50,
-      displayRestricted: false
-    };
-
-    const searchRequest: JobAdvertisementSearchRequest = {
-      page: 0,
-      size: ITEMS_PER_PAGE,
-      sort: this.mapper.mapSort(filtersValues.sort),
-      body: body
-    };
-    this.resultList$ = this.jobAdsService.search(searchRequest);
+    this.filtersChange$.next(filtersValues);
   }
 
   onScroll(e) {
-    // also triggering subject.next
-    console.log(e);
+    this.scroll$.next(this.scroll$.getValue() + 1);
   }
 }
