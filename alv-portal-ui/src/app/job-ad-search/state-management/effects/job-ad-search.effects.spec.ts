4e58dc6394947375ddea2803102f10ea03a8c415
Marton Biro <marton.biro@mimacom.com>
2018-12-05 21:35:14 +0100

    Add marble test for JobAdSearchEffects.initJobSearch$

--- alv-portal-ui/src/app/job-ad-search/state-management/effects/job-ad-search.effects.spec.ts
+++ alv-portal-ui/src/app/job-ad-search/state-management/effects/job-ad-search.effects.spec.ts
@@ -5,51 +5,90 @@ import {
 } from './job-ad-search.effects';
 import { Actions } from '@ngrx/effects';
 import { JobAdvertisementRepository } from '../../../shared/backend-services/job-advertisement/job-advertisement.repository';
-import { initialState, JobAdSearchState } from '../state/job-ad-search.state';
+import { JobAdSearchState } from '../state/job-ad-search.state';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { getTestScheduler, hot } from 'jasmine-marbles';
-import { InitResultListAction } from '../actions/job-ad-search.actions';
+import { cold, getTestScheduler, hot } from 'jasmine-marbles';
+import {
+  ApplyFilterAction,
+  FilterAppliedAction,
+  InitResultListAction
+} from '../actions/job-ad-search.actions';
+import { Observable } from 'rxjs/index';
+import { JobAdvertisement } from '../../../shared/backend-services/job-advertisement/job-advertisement.types';
+import { jobAdSearchReducer } from '../reducers/job-ad-search.reducers';
+import { JobSearchFilter } from '../../job-search-filter.types';
+import SpyObj = jasmine.SpyObj;
 
 describe('JobAdSearchEffects', () => {
   let jobAdSearchEffects: JobAdSearchEffects;
 
-  let mockActions$: Actions;
-  let mockJobAdsService: JobAdvertisementRepository;
-  let mockStore: Store<JobAdSearchState>;
-  let mockRouter: Router;
+  let store: Store<JobAdSearchState>;
+  let actions$: Observable<any>;
+  let jobAdService: SpyObj<JobAdvertisementRepository>;
+  let router: Router;
 
   beforeEach(() => {
-    mockRouter = jasmine.createSpyObj('mockRouter', ['navigate']);
-    mockStore = jasmine.createSpyObj('mockStore', ['pipe', 'dispatch']);
-    mockJobAdsService = jasmine.createSpyObj('mockJobAdsService', ['search', 'findById']);
+    router = jasmine.createSpyObj('mockRouter', ['navigate']);
+    jobAdService = jasmine.createSpyObj('mockJobAdsService', ['search', 'findById']);
 
     TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({ 'jobAdSearch': jobAdSearchReducer })
+      ],
       providers: [
         JobAdSearchEffects,
-        provideMockActions(() => mockActions$),
-        { provide: Router, useValue: mockRouter },
-        { provide: Store, useValue: mockStore },
-        { provide: JobAdvertisementRepository, useValue: mockJobAdsService },
-        { provide: Router, useValue: mockRouter },
-        { provide: Store, useValue: mockStore },
+        provideMockActions(() => actions$),
+        { provide: Router, useValue: router },
+        { provide: JobAdvertisementRepository, useValue: jobAdService },
         { provide: JOB_AD_SEARCH_EFFECTS_DEBOUNCE, useValue: 30 },
         { provide: JOB_AD_SEARCH_EFFECTS_SCHEDULER, useFactory: getTestScheduler },
       ]
     });
 
     jobAdSearchEffects = TestBed.get(JobAdSearchEffects);
-
+    actions$ = TestBed.get(Actions);
+    store = TestBed.get(Store);
   });
 
   describe('initJobSearch$', () => {
     const initResultListAction = new InitResultListAction();
 
-    it('should work', () => {
-      // todo: implement
-      expect(true).toBe(true);
+    it('should return a new FilterAppliedAction on success, and completes', () => {
+      const jobAd: any = { id: 1 };
+      const result = [jobAd as JobAdvertisement];
+
+      const jobAdSearchResult = {
+        totalCount: 10,
+        result
+      };
+
+      actions$ = hot('-a--a-', { a: initResultListAction });
+      jobAdService.search.and.returnValue(cold('--b|', { b: jobAdSearchResult }));
+
+      const expected = cold('---c|-', {
+        c: new FilterAppliedAction({
+          page: result,
+          totalCount: 10
+        })
+      });
+
+      expect(jobAdSearchEffects.initJobSearch$).toBeObservable(expected);
+    });
+
+    it('should complete after ApplyFilterAction', () => {
+
+      actions$ = hot('-a-b--b-', {
+        a: new ApplyFilterAction({} as JobSearchFilter),
+        b: initResultListAction
+      });
+      jobAdService.search.and.returnValue(cold('--c|', { c: {} }));
+
+      const expected = cold('-|--');
+
+      expect(jobAdSearchEffects.initJobSearch$).toBeObservable(expected);
     });
 
   });
