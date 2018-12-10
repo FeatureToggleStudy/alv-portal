9f3d0a92a2e234e71373df1c93a5c6503fc9aa84
Patrick Dobler <patrick.dobler@gmail.com>
2018-12-10 20:47:41 +0100

    not needed to declare service as provider AND as @Injectable root

--- alv-portal-ui/src/app/shared/backend-services/backend-services.module.ts
+++ alv-portal-ui/src/app/shared/backend-services/backend-services.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OccupationPresentationService } from './reference-service/occupation-presentation.service';
-import { OccupationLabelService } from './reference-service/occupation-label.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [
-    OccupationPresentationService,
-    OccupationLabelService,
-  ]
+  providers: []
 })
 export class BackendServicesModule {
 }
