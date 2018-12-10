ea7acae84dbc20a85602048d464fdde4ceea58a0
Iurii Katkov <iurii.katkov@mimacom.com>
2018-12-10 18:10:24 +0100

    reference service for occupation

--- alv-portal-ui/src/app/shared/backend-services/backend-services.module.ts
+++ alv-portal-ui/src/app/shared/backend-services/backend-services.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { OccupationPresentationService } from './reference-service/occupation-presentation.service';
+import { OccupationLabelService } from './reference-service/occupation-label.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    OccupationPresentationService,
+    OccupationLabelService,
+  ]
 })
 export class BackendServicesModule {
 }
