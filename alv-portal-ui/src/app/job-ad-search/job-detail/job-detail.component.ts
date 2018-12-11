04ef1d3bccb391913940c537c7334bfc762d2665
Iurii Katkov <iurii.katkov@mimacom.com>
2018-12-11 11:16:06 +0100

    undefined error fixed

--- alv-portal-ui/src/app/job-ad-search/job-detail/job-detail.component.ts
+++ alv-portal-ui/src/app/job-ad-search/job-detail/job-detail.component.ts
@@ -73,8 +73,11 @@ export class JobDetailComponent extends AbstractSubscriber implements OnInit {
         map(([job, currentLanguage]) =>
           JobAdvertisementUtils.getJobDescription(job, currentLanguage))
       );
+    const jobWithOccupation$ = this.job$.pipe(
+      filter(job => job.jobContent.occupations && !!job.jobContent.occupations.length)
+    );
 
-    this.occupation$ = combineLatest(this.job$, this.i18nService.currentLanguage$)
+    this.occupation$ = combineLatest(jobWithOccupation$, this.i18nService.currentLanguage$)
       .pipe(
         flatMap(([job, currentLanguage]) => {
           return this.occupationPresentationService.findOccupationLabelsByAvamCode(+job.jobContent.occupations[0].avamOccupationCode, currentLanguage);
