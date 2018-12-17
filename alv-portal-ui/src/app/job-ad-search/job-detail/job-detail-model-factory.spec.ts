e01d45f945a0ad2e9b57fcaa26305f104cb1116e
Iurii Katkov <iurii.katkov@mimacom.com>
2018-12-17 10:27:14 +0100

    small typo in marble test

--- alv-portal-ui/src/app/job-ad-search/job-detail/job-detail-model-factory.spec.ts
+++ alv-portal-ui/src/app/job-ad-search/job-detail/job-detail-model-factory.spec.ts
@@ -159,9 +159,8 @@ describe('JobDetailModelFactory', () => {
       mockJobAd
     );
     const expected = cold('---r--', { r: expectedJobDetailModel });
-
-
-    expect(jobDetailModel).toBeObservable(expected, {});
+    
+    expect(jobDetailModel).toBeObservable(expected);
   });
 })
 ;
