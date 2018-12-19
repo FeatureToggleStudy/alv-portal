9e171da17051b16dd38467c869dcf05dec283086
Marton Biro <marton.biro@mimacom.com>
2018-12-19 07:19:16 +0100

    DF-391: Add InitResultListAction

--- alv-portal-ui/src/app/candidate-search/state-management/actions/candidate-search.actions.ts
+++ alv-portal-ui/src/app/candidate-search/state-management/actions/candidate-search.actions.ts
@@ -1,6 +1,8 @@
 import { Action } from '@ngrx/store';
+import { CandidateSearchFilter } from '../state/candidate-search.state';
 
 export const INIT_RESULT_LIST = 'CANDIDATES:INIT_RESULT_LIST';
+export const APPLY_FILTER = 'CANDIDATES:APPLY_FILTER';
 
 export class InitResultListAction implements Action {
   readonly type = INIT_RESULT_LIST;
@@ -9,5 +11,13 @@ export class InitResultListAction implements Action {
   }
 }
 
+export class ApplyFilterAction implements Action {
+  readonly type = APPLY_FILTER;
+
+  constructor(public payload: CandidateSearchFilter) {
+  }
+}
+
 export type Actions = InitResultListAction
+  | ApplyFilterAction
   ;
