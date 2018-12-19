7882000841a2833173ff5f9744a664f98b084df2
Patrick Dobler <patrick.dobler@gmail.com>
2018-12-19 16:31:30 +0100

    fix deserialization of localities and keywords

--- alv-portal-ui/src/app/job-ad-search/job-search/job-search-filter-parameter.service.ts
+++ alv-portal-ui/src/app/job-ad-search/job-search/job-search-filter-parameter.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { JobSearchFilter } from '../state-management/state/job-search-filter.types';
 import { Base64Service } from '../../core/base64.service';
 import { OccupationMultiTypeaheadItem } from '../../shared/occupations/occupation-multi-typeahead-item';
+import { SimpleMultiTypeaheadItem } from '../../shared/forms/input/multi-typeahead/simple-multi-typeahead.item';
 
 @Injectable()
 export class JobSearchFilterParameterService {
@@ -18,6 +19,14 @@ export class JobSearchFilterParameterService {
     if (jobSearchFilter.occupations) {
       jobSearchFilter.occupations = jobSearchFilter.occupations
         .map((a) => OccupationMultiTypeaheadItem.fromJson(a));
+    }
+    if (jobSearchFilter.localities) {
+      jobSearchFilter.localities = jobSearchFilter.localities
+        .map((a) => SimpleMultiTypeaheadItem.fromJson(a));
+    }
+    if (jobSearchFilter.keywords) {
+      jobSearchFilter.keywords = jobSearchFilter.keywords
+        .map((a) => SimpleMultiTypeaheadItem.fromJson(a));
     }
     return jobSearchFilter;
   }
