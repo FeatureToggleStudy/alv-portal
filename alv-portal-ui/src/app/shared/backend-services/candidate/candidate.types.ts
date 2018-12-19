750fd6b20b88e76eacc1815652116d8f990f3bee
Marton Biro <marton.biro@mimacom.com>
2018-12-19 11:46:52 +0100

    DF-391: Add candidate types

--- alv-portal-ui/src/app/shared/backend-services/candidate/candidate.types.ts
+++ alv-portal-ui/src/app/shared/backend-services/candidate/candidate.types.ts
@@ -1,3 +1,15 @@
+import {
+  Availability,
+  Contact,
+  Degree,
+  Experience,
+  Gender,
+  Graduation,
+  ISCED_1997,
+  LanguageSkill
+} from '../shared.types';
+import { Occupation } from '../job-advertisement/job-advertisement.types';
+
 export interface CandidateSearchRequest {
   page: number;
   size: number;
@@ -16,5 +28,54 @@ export interface CandidateSearchRequestBody {
 
 export interface Candidate {
   id: string;
-  // todo: add implementation
+  firstName: string;
+  lastName: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  address: Address;
+  nationalityCode: string;
+  candidateProfile: CandidateProfile;
+}
+
+export interface CandidateProfile {
+  id: string;
+  gender: Gender;
+  availability: Availability;
+  externalId: string;
+  residenceCantonCode: string;
+  workLoad: number;
+  isPublic: boolean;
+  isProtected: boolean;
+  showProtectedData: boolean;
+  workForm: string[];
+  preferredWorkRegions: string[];
+  preferredWorkCantons: string[];
+  jobExperiences: JobExperience[];
+  languages: LanguageSkill[];
+  drivingCategories: string[];
+  highestEducationLevel: string;
+  highestDegree: string;
+  jobCenterCode: string;
+  jobAdvisor: Contact;
+  visited?: boolean;
+  contactTypes?: string[];
+}
+
+export interface Address {
+  street: string;
+  zipCode: string;
+  city: string;
+}
+
+export interface JobExperience {
+  occupation: Occupation,
+  occupationLabel: string;
+  experience: Experience;
+  graduation: Graduation;
+  degree: Degree;
+  education: ISCED_1997;
+  remark: string;
+  lastJob: boolean;
+  wanted: boolean;
 }
