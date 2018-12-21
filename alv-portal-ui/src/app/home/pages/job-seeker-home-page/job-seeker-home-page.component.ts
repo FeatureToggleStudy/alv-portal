4a99e1a9ff4f710e23467df27bba5b1ae9cdc107
Iurii Katkov <iurii.katkov@mimacom.com>
2018-12-21 15:58:23 +0100

    css for the widgets

--- alv-portal-ui/src/app/home/pages/job-seeker-home-page/job-seeker-home-page.component.ts
+++ alv-portal-ui/src/app/home/pages/job-seeker-home-page/job-seeker-home-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alv-job-seeker-home-page',
@@ -7,16 +6,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./job-seeker-home-page.component.scss']
 })
 export class JobSeekerHomePageComponent implements OnInit {
-  findPositionsForm: FormGroup;
-
-
-  constructor(private fb: FormBuilder) {
-  }
 
   ngOnInit() {
-    this.findPositionsForm = this.fb.group({
-      profession: this.fb.control('Designer'),
-      canton: this.fb.control('Zurich')
-    });
   }
 }
