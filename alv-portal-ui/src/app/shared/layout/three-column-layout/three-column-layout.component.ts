3ddf6f1fac760a9edb8a960bbe89da900680f238
Sven Heinrich <sven.heinrich@mimacom.com>
2018-12-27 19:55:42 +0100

    Implement feedback from review

--- alv-portal-ui/src/app/shared/layout/three-column-layout/three-column-layout.component.ts
+++ alv-portal-ui/src/app/shared/layout/three-column-layout/three-column-layout.component.ts
@@ -1,27 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'alv-three-column-layout',
   templateUrl: './three-column-layout.component.html',
   styleUrls: ['./three-column-layout.component.scss']
 })
-export class ThreeColumnLayoutComponent implements OnInit {
+export class ThreeColumnLayoutComponent implements OnInit, OnDestroy {
 
-  @Input() mobileOrdering: number[];
+  /**
+   * Redefines the column ordering for mobile. e.g. [1, 3, 2] will display the third
+   * column as the second column on mobile.
+   */
+  @Input() mobileOrdering = [1, 2, 3];
 
+  /**
+   * This value indicates the top margin of the component in pixels within the <main>
+   * element. It's needed for proper sticky positioning.
+   */
   @Input() stickyTop = 0;
 
+  /**
+   * This is the small (sm) breakpoint copied from the bootstrap 4 library
+   */
   private readonly BOOTSTRAP_BREAKPOINT_SM = '(max-width: 767.98px)';
+
+  /**
+   * This value equals to number 4 of the bootstrap default sizes (e.g. .mt-md-4, .m-4, etc.)
+   */
+  private readonly BOOTSTRAP_SIZE_4 = '1.5rem';
 
   constructor() {
   }
 
   ngOnInit() {
-    if (!this.mobileOrdering) {
-      this.mobileOrdering = [1, 2, 3];
-    }
+    window.addEventListener('resize', this.setSidePanelHeight);
     this.setSidePanelHeight();
-    this.listenToWindowResize();
   }
 
   getColumnClass(columnIndex) {
@@ -31,15 +44,12 @@ export class ThreeColumnLayoutComponent implements OnInit {
     return 'order-' + this.mobileOrdering[columnIndex];
   }
 
-  private listenToWindowResize() {
-    window.addEventListener('resize', (event) => {
-      this.setSidePanelHeight();
-    });
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.setSidePanelHeight);
   }
 
   private setSidePanelHeight() {
     const mainContainer = document.querySelector('main');
-    const marginTop = '1.5rem';
     const isMobileViewPort = window.matchMedia(this.BOOTSTRAP_BREAKPOINT_SM);
 
     document.querySelectorAll('alv-three-column-layout .side-panel').forEach(sidePanel => {
@@ -47,8 +57,8 @@ export class ThreeColumnLayoutComponent implements OnInit {
         sidePanel.setAttribute('style', '');
       } else {
         sidePanel.setAttribute('style',
-          `height: calc(${mainContainer.clientHeight - this.stickyTop}px - ${marginTop});
-                 top: calc(${marginTop} + ${this.stickyTop}px)`);
+          `height: calc(${mainContainer.clientHeight - this.stickyTop}px - ${this.BOOTSTRAP_SIZE_4});
+                 top: calc(${this.BOOTSTRAP_SIZE_4} + ${this.stickyTop}px)`);
       }
     });
   }
