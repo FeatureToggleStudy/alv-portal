f230708f77bc8c9165964d138419e688b068cbe4
Iurii Katkov <iurii.katkov@mimacom.com>
2018-12-27 14:27:38 +0100

    candidate last job title - proper algorithm

--- alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail-model-factory.ts
+++ alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail-model-factory.ts
@@ -5,38 +5,47 @@ import {
   JobExperience
 } from '../../shared/backend-services/candidate/candidate.types';
 import { flatMap, map } from 'rxjs/operators';
-import {
-  OccupationLabelRepository,
-  OccupationTypes
-} from '../../shared/backend-services/reference-service/occupation-label.repository';
+import { OccupationLabelRepository, } from '../../shared/backend-services/reference-service/occupation-label.repository';
 import { I18nService } from '../../core/i18n.service';
-import { OccupationLabelData } from '../../shared/backend-services/reference-service/occupation-label.types';
 import { CandidateDetailModel } from './candidate-detail-model';
+import { extractGenderAwareTitle, findRelevantJobExperience } from '../candidate-rules';
+import {
+  GenderAwareOccupationLabel,
+  OccupationService
+} from '../../shared/occupations/occupation.service';
 
 @Injectable()
 export class CandidateDetailModelFactory {
 
   constructor(private occupationLabelRepository: OccupationLabelRepository,
-              private i18nService: I18nService) {
+              private i18nService: I18nService,
+              private occupationService: OccupationService) {
 
   }
 
-  private static getLastJob(candidateProfile: CandidateProfile) {
-    return candidateProfile.jobExperiences[0];
-  }
 
   public create(candidateProfile$: Observable<CandidateProfile>): Observable<CandidateDetailModel> {
 
-    const lastJob$ = candidateProfile$.pipe(map(CandidateDetailModelFactory.getLastJob));
-    const lastJobOccupationLabel$: Observable<OccupationLabelData> = combineLatest(lastJob$, this.i18nService.currentLanguage$).pipe(
+    const lastJob$ = candidateProfile$.pipe(map(c => findRelevantJobExperience(c)));
+    const lastJobOccupationLabel$: Observable<GenderAwareOccupationLabel> = combineLatest(lastJob$, this.i18nService.currentLanguage$).pipe(
       flatMap((jobExperienceAndLang: [JobExperience, string]) => {
-          const avamCode = String(jobExperienceAndLang[0].occupation.avamCode);
-          const language = jobExperienceAndLang[1];
-          return this.occupationLabelRepository.getOccupationLabelsByKey(OccupationTypes.AVAM, avamCode, language);
+          const language: string = jobExperienceAndLang[1];
+          const jobExperience: JobExperience = jobExperienceAndLang[0];
+          const professionCode = CandidateDetailModelFactory.extractProfessionCode(jobExperience);
+          return this.occupationService.findLabel(professionCode, language);
         }
       ));
     return combineLatest(candidateProfile$, lastJobOccupationLabel$).pipe(
-      map((both) => new CandidateDetailModel(both[0], both[1]))
+      map((both: [CandidateProfile, GenderAwareOccupationLabel]) => {
+        return new CandidateDetailModel(both[0], extractGenderAwareTitle(both[0], both[1]));
+      })
     );
+  }
+
+  private static extractProfessionCode(jobExperience: JobExperience) {
+    return {
+      value: String(jobExperience.occupation.avamCode),
+      type: 'AVAM'
+    };
   }
 }
--- alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail-model.ts
+++ alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail-model.ts
@@ -1,5 +1,4 @@
 import { CandidateProfile } from '../../shared/backend-services/candidate/candidate.types';
-import { OccupationLabelData } from '../../shared/backend-services/reference-service/occupation-label.types';
 
 /**
  * A "view-model" for the Candidate-Detail Page that has all of our business logic in it to
@@ -7,7 +6,7 @@ import { OccupationLabelData } from '../../shared/backend-services/reference-ser
  */
 export class CandidateDetailModel {
   constructor(public candidateProfile: CandidateProfile,
-              public lastJobOccupationLabel: OccupationLabelData) {
+              public lastJobOccupationLabel: string) {
 
   }
 }
