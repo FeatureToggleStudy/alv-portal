feb2b0f74436d8e78a568f27d4a60abd1bc133b3
Sven Heinrich <sven.heinrich@mimacom.com>
2018-12-27 15:42:45 +0100

    Simple solution using JS for scrolling issue

--- alv-portal-ui/src/app/shared/layout/three-column-layout/three-column-layout.component.ts
+++ alv-portal-ui/src/app/shared/layout/three-column-layout/three-column-layout.component.ts
@@ -9,12 +9,15 @@ export class ThreeColumnLayoutComponent implements OnInit {
 
   @Input() mobileOrdering: number[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     if (!this.mobileOrdering) {
       this.mobileOrdering = [1, 2, 3];
     }
+    this.setSidePanelHeight();
+    this.listenToWindowResize();
   }
 
   getColumnClass(columnIndex) {
@@ -24,4 +27,15 @@ export class ThreeColumnLayoutComponent implements OnInit {
     return 'order-' + this.mobileOrdering[columnIndex];
   }
 
+  private listenToWindowResize() {
+    window.addEventListener('resize', (event) => {
+      this.setSidePanelHeight();
+    });
+  }
+
+  private setSidePanelHeight() {
+    document.querySelectorAll('.side-panel').forEach(sidePanel => {
+      sidePanel.setAttribute('style', `height: ${document.querySelector('main').clientHeight - 100}px`);
+    });
+  }
 }
