3857fbcda7ddccc54b6448951084eeae3c0130f8
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-03 16:21:40 +0100

    marbles

---
+++ alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail-model-factory.spec.ts
@@ -0,0 +1,147 @@
+import { JobCenterRepository } from '../../shared/backend-services/reference-service/job-center.repository';
+import { cold } from 'jasmine-marbles';
+import { async, TestBed } from '@angular/core/testing';
+import { I18nService } from '../../core/i18n.service';
+import { mockJobCenter } from '../../shared/backend-services/reference-service/job-center.mock';
+import { CandidateDetailModelFactory } from './candidate-detail-model-factory';
+import { OccupationLabelRepository } from '../../shared/backend-services/reference-service/occupation-label.repository';
+import { CandidateRepository } from '../../shared/backend-services/candidate/candidate.repository';
+import { AuthenticationService } from '../../core/auth/authentication.service';
+import { OccupationLabelData } from '../../shared/backend-services/reference-service/occupation-label.types';
+import { CandidateProtectedData } from '../../shared/backend-services/candidate/candidate.types';
+import { RegistrationStatus, User, UserRole } from '../../core/auth/user.model';
+import {
+  mockCandidateProfile,
+  mockJobExperience
+} from '../../shared/backend-services/candidate/candidate.mock';
+import { of } from 'rxjs';
+import { CandidateDetailModel, JobExperienceModel } from './candidate-detail-model';
+import { Contact } from '../../shared/backend-services/shared.types';
+import { map } from 'rxjs/operators';
+
+describe('CandidateDetailModelFactory', () => {
+  let candidateDetailModelFactory: CandidateDetailModelFactory;
+  let i18nServiceMock;
+  let occupationLabelRepositoryMock;
+  let referenceServiceRepositoryMock: JobCenterRepository;
+  let candidateRepositoryMock;
+  let authenticationServiceMock;
+
+  const LANGUAGE_VALUES = {
+    e: 'en',
+    f: 'fr',
+    d: 'de',
+    i: 'i'
+  };
+  const occupationLabelDataMock: OccupationLabelData = {
+    default: 'programmer/ka',
+    m: 'programmer',
+    f: 'programmerka'
+  };
+
+  const mockProtectedData: CandidateProtectedData = {
+    id: '11',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    phone: '+41211212',
+    mobile: '+41211212',
+    email: 'string@dd.com',
+    address: {
+      street: 'street',
+      zipCode: '2002',
+      city: 'Bern'
+    },
+    nationalityCode: 'de',
+    candidateProfile: null
+  };
+
+  const userMock: User = {
+    id: '234',
+    login: 'qwert',
+    email: 'asdsf@gma.com',
+    langKey: 'en',
+    firstName: 'Name',
+    lastName: 'lastname',
+    authorities: [UserRole.ROLE_PAV],
+    registrationStatus: RegistrationStatus.REGISTERED,
+    hasAnyAuthorities(authorities: Array<UserRole>): boolean {
+      return true;
+    },
+    isRegistered(): boolean {
+      return true;
+    },
+    isAdmin(): boolean {
+      return false;
+    }
+  };
+
+  const mockContact: Contact = {
+    salutation: 'mr',
+    firstName: 'ads',
+    lastName: 'las',
+    phone: '+213231212',
+    email: 'oooa@gma.com'
+  };
+
+
+  const mockJobExperienceModel: JobExperienceModel = {
+    jobExperience: mockJobExperience,
+    occupationLabel: 'string',
+    displayGraduation: true,
+    displayDegree: true,
+  };
+
+
+  beforeEach(() => {
+    i18nServiceMock = {
+      currentLanguage$: cold('--e--', LANGUAGE_VALUES)
+    };
+
+    referenceServiceRepositoryMock = jasmine.createSpyObj('ReferenceServiceRepository', {
+      'resolveJobCenter': cold('-x|', { x: mockJobCenter })
+    });
+
+    occupationLabelRepositoryMock = jasmine.createSpyObj('OccupationLabelRepository', {
+      'getOccupationLabelsByKey': cold('-x|', { x: occupationLabelDataMock })
+    });
+
+    candidateRepositoryMock = jasmine.createSpyObj('CandidateRepository', {
+      'getCandidateProtectedData': cold('-x|', { x: mockProtectedData })
+    });
+    authenticationServiceMock = jasmine.createSpyObj('AuthenticationService', {
+      'getCurrentUser': cold('-x|', { x: userMock })
+    });
+
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        CandidateDetailModelFactory,
+        { provide: I18nService, useValue: i18nServiceMock },
+        { provide: JobCenterRepository, useValue: referenceServiceRepositoryMock },
+        { provide: OccupationLabelRepository, useValue: occupationLabelRepositoryMock },
+        { provide: CandidateRepository, useValue: candidateRepositoryMock },
+        { provide: AuthenticationService, useValue: authenticationServiceMock }
+      ]
+    });
+    candidateDetailModelFactory = TestBed.get(CandidateDetailModelFactory);
+  });
+
+  it('should create CandidateDetailModelFactory ', () => {
+    expect(candidateDetailModelFactory).toBeTruthy();
+  });
+
+  it('should create a CandidateDetail model object', async(() => {
+    const candidateDetailModel = candidateDetailModelFactory.create(of(mockCandidateProfile));
+
+    const expectedCandidateDetailModel = new CandidateDetailModel(
+      mockCandidateProfile,
+      mockJobCenter,
+      [mockJobExperienceModel],
+      mockProtectedData,
+      mockContact
+    );
+    const expected = cold('---r--', { r: null });
+
+    expect(candidateDetailModel.pipe(map(() => null))).toBeObservable(expected);
+  }));
+});
---
+++ alv-portal-ui/src/app/shared/backend-services/candidate/candidate.mock.ts
@@ -0,0 +1,64 @@
+import {
+  CandidateProfile,
+  JobExperience,
+  LanguageSkill,
+  Occupation
+} from './candidate.types';
+import {
+  Availability,
+  CEFR_Level,
+  Degree,
+  Experience,
+  Gender,
+  Graduation,
+  ISCED_1997
+} from '../shared.types';
+
+export const mockOccupation: Occupation = {
+  avamCode: 2323,
+  bfsCode: 12123,
+  sbn3Code: 121212,
+  sbn5Code: 31312,
+};
+
+
+export const mockJobExperience: JobExperience = {
+    occupation: mockOccupation,
+    experience: Experience.LESS_THAN_1_YEAR,
+    graduation: Graduation.ACCEPTED,
+    degree: Degree.SEK_II_BERUFSMATURITAET,
+    education: ISCED_1997.ISCED1,
+    remark: 'string',
+    lastJob: true,
+    wanted: true
+  }
+;
+
+export const languageSkillMock: LanguageSkill = {
+  code: 'ru',
+  spokenLevel: CEFR_Level.BASIC,
+  writtenLevel: CEFR_Level.INTERMEDIATE
+};
+
+export const mockCandidateProfile: CandidateProfile = {
+  id: 'something',
+  gender: Gender.FEMALE,
+  availability: Availability.IMMEDIATE,
+  externalId: 'something',
+  residenceCantonCode: 'something',
+  workLoad: 100,
+  isPublic: true,
+  isProtected: true,
+  showProtectedData: true,
+  workForm: ['one', 'two'],
+  preferredWorkRegions: ['one', 'two'],
+  preferredWorkCantons: ['one', 'two'],
+  jobExperiences: [mockJobExperience],
+  languages: [languageSkillMock],
+  drivingCategories: ['one', 'two'],
+  highestEducationLevel: 'something',
+  highestDegree: 'something',
+  jobCenterCode: 'something',
+  jobAdvisor: null,
+  contactTypes: ['one', 'two'],
+};
