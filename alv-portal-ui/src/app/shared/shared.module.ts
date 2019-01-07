953bf866729f2b18eaf1c56966d9293eb008a401
Wojciech Lebek <wojciech.lebek@mimacom.com>
2019-01-07 14:17:47 +0100

    [DF-180] Added a landing page guard in AlV Portal: formatting corrections

--- alv-portal-ui/src/app/shared/shared.module.ts
+++ alv-portal-ui/src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { PrettyJsonModule } from 'angular2-prettyjson';
-import { TranslateModule } from '@ngx-translate/core';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { LayoutModule } from './layout/layout.module';
-import { FormsModule as AlvFormsModule } from './forms/forms.module';
-import { SharedAuthModule } from './auth/shared-auth.module';
-import { ClipboardModule } from 'ngx-clipboard';
-import { CommonModule } from '@angular/common';
-import { MarkdownModule } from 'ngx-markdown';
-import { PipesModule } from './pipes/pipes.module';
-import { GeoLocationSelectionComponent } from './localities/geo-location-selection/geo-location-selection.component';
+import {NgModule} from '@angular/core';
+import {PrettyJsonModule} from 'angular2-prettyjson';
+import {TranslateModule} from '@ngx-translate/core';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {LayoutModule} from './layout/layout.module';
+import {FormsModule as AlvFormsModule} from './forms/forms.module';
+import {SharedAuthModule} from './auth/shared-auth.module';
+import {ClipboardModule} from 'ngx-clipboard';
+import {CommonModule} from '@angular/common';
+import {MarkdownModule} from 'ngx-markdown';
+import {PipesModule} from './pipes/pipes.module';
+import {GeoLocationSelectionComponent} from './localities/geo-location-selection/geo-location-selection.component';
 import {LandingPageGuard} from "./landing-page/landing-page.guard";
 
 @NgModule({
@@ -39,7 +39,8 @@ import {LandingPageGuard} from "./landing-page/landing-page.guard";
     MarkdownModule,
     SharedAuthModule,
     GeoLocationSelectionComponent,
-  providers:[
+  ],
+  providers: [
     LandingPageGuard
   ]
 })
