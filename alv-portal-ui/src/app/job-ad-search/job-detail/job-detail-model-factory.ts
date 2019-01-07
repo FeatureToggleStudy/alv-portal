1bfef30c5b9da16dd7bd96fae826177ccae73f12
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-07 16:59:00 +0100

    refactor of job-detail model

--- alv-portal-ui/src/app/job-ad-search/job-detail/job-detail-model-factory.ts
+++ alv-portal-ui/src/app/job-ad-search/job-detail/job-detail-model-factory.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { JobAdvertisement } from '../../shared/backend-services/job-advertisement/job-advertisement.types';
-import { combineLatest, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { flatMap, map, withLatestFrom } from 'rxjs/operators';
 import { JobAdvertisementUtils } from '../../shared/backend-services/job-advertisement/job-advertisement.utils';
 import { I18nService } from '../../core/i18n.service';
@@ -14,23 +14,22 @@ export class JobDetailModelFactory {
   constructor(private i18nService: I18nService, private referenceServiceRepository: JobCenterRepository) {
   }
 
-  public create(job$: Observable<JobAdvertisement>): Observable<JobDetailModel> {
+  public create(job: JobAdvertisement): Observable<JobDetailModel> {
 
-    const jobCenter$ = combineLatest(job$, this.i18nService.currentLanguage$)
-      .pipe(
-        flatMap(([job, currentLanguage]) => {
-          if (job.jobCenterCode) {
-            return this.referenceServiceRepository.resolveJobCenter(job.jobCenterCode, currentLanguage);
-          }
-          return of(null);
-        })
-      );
+    const jobCenter$ = this.i18nService.currentLanguage$.pipe(
+      flatMap((currentLanguage) => {
+        if (job.jobCenterCode) {
+          return this.referenceServiceRepository.resolveJobCenter(job.jobCenterCode, currentLanguage);
+        }
+        return of(null);
+      })
+    );
 
 
-    return combineLatest(job$, jobCenter$)
+    return jobCenter$
       .pipe(
         withLatestFrom(this.i18nService.currentLanguage$),
-        map(([[job, jobCenter], currentLanguage]) => {
+        map(([jobCenter, currentLanguage]) => {
           const jobDescription = JobAdvertisementUtils.getJobDescription(job, currentLanguage);
           return new JobDetailModel(jobDescription, jobCenter, job);
         })
