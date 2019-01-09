ab98e30adbb61bbafeed1ea1035cb89a93b810df
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-09 14:15:34 +0100

    JobAdManagementRowComponent data

--- alv-portal-ui/src/app/manage-job-ads/manage-job-ad-search/manage-job-ad-search.component.ts
+++ alv-portal-ui/src/app/manage-job-ads/manage-job-ad-search/manage-job-ad-search.component.ts
@@ -3,6 +3,9 @@ import { getManagedJobAdResults, ManageJobAdsState } from '../state-management/s
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { JobAdvertisement } from '../../shared/backend-services/job-advertisement/job-advertisement.types';
+import { JobAdManagementRow } from './job-ad-management-row/job-ad-management-row';
+import { I18nService } from '../../core/i18n.service';
+import { map, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'alv-manage-job-ad-search',
@@ -13,11 +16,26 @@ export class ManageJobAdSearchComponent implements OnInit {
 
   private jobSearchResults$: Observable<JobAdvertisement[]>;
 
-  constructor(private store: Store<ManageJobAdsState>) {
+  jobAdManagementRows$: Observable<JobAdManagementRow[]>;
+
+  currentLanguage: Observable<string>;
+
+
+  constructor(private store: Store<ManageJobAdsState>,
+              private i18nService: I18nService) {
   }
 
   ngOnInit() {
+    this.currentLanguage = this.i18nService.currentLanguage$;
     this.jobSearchResults$ = this.store.pipe(select(getManagedJobAdResults));
+    this.jobAdManagementRows$ = this.jobSearchResults$.pipe(
+      withLatestFrom(this.currentLanguage),
+      map(([jobSearchResults, currentLanguage]) =>
+        jobSearchResults.map(jobSearchResult =>
+          new JobAdManagementRow(jobSearchResult, currentLanguage)
+        )
+      )
+    )
   }
 
 }
