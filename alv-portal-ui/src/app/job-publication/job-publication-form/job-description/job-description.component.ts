31e4f1274d55c2b239ea7f679b9b660b6e730222
Marton Biro <marton.biro@mimacom.com>
2019-01-09 13:33:12 +0100

    DF479: Add job-description section

--- alv-portal-ui/src/app/job-publication/job-publication-form/job-description/job-description.component.ts
+++ alv-portal-ui/src/app/job-publication/job-publication-form/job-description/job-description.component.ts
@@ -7,21 +7,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./job-description.component.scss']
 })
 export class JobDescriptionComponent implements OnInit {
-
-  readonly TITLE_MAX_LENGTH = 255;
-
-  readonly NUMBER_OF_JOBS_MIN = 1;
-
-  readonly NUMBER_OF_JOBS_MAX = 99;
-
-  readonly DESCRIPTION_MAX_LENGTH = 10000;
-
-
   @Input()
   jobPublicationForm: FormGroup;
 
   jobDescriptionGroup: FormGroup;
-
 
   constructor(private fb: FormBuilder) {
   }
@@ -29,18 +18,8 @@ export class JobDescriptionComponent implements OnInit {
   ngOnInit(): void {
     //todo: Set initial value
     this.jobDescriptionGroup = this.fb.group({
-      title: ['', [
-        Validators.required,
-        Validators.maxLength(this.TITLE_MAX_LENGTH)
-      ]],
-      numberOfJobs: [1, [
-        Validators.required,
-        Validators.min(this.NUMBER_OF_JOBS_MIN),
-        Validators.max(this.NUMBER_OF_JOBS_MAX)
-      ]],
-      jobDescription: ['', [
-        Validators.maxLength(this.DESCRIPTION_MAX_LENGTH)
-      ]]
+      title: ['', Validators.required],
+      numberOfJobs: [''],
     });
 
     this.jobPublicationForm.addControl('jobDescription', this.jobDescriptionGroup);
