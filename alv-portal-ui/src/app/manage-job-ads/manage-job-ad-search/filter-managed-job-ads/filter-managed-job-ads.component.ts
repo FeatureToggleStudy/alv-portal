d342e94f89a3ac490d6b3b1202621195b01500e2
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-10 13:52:31 +0100

    alv-select

--- alv-portal-ui/src/app/manage-job-ads/manage-job-ad-search/filter-managed-job-ads/filter-managed-job-ads.component.ts
+++ alv-portal-ui/src/app/manage-job-ads/manage-job-ad-search/filter-managed-job-ads/filter-managed-job-ads.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { SelectableOption } from '../../../shared/forms/input/selectable-option.model';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'alv-filter-managed-job-ads',
@@ -10,15 +12,45 @@ export class FilterManagedJobAdsComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  onlineSinceOptions$: Observable<SelectableOption[]> = of([
+    {
+      label: 'dashboard.job-publication.publication-period.week',
+      value: 7
+    },
+    {
+      label: 'dashboard.job-publication.publication-period.month',
+      value: 30
+    },
+    {
+      label: 'dashboard.job-publication.publication-period.months',
+      value: 90
+    },
+    {
+      label: 'dashboard.job-publication.publication-period.months',
+      value: 180
+    },
+    {
+      label: 'dashboard.job-publication.publication-period.year',
+      value: 365
+    },
+    {
+      label: 'dashboard.job-publication.publication-period.all',
+      value: null
+    }
+  ]);
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
     this.form = this.fb.group({
       onlineSinceDays: ['']
     });
   }
-  filter () {
+
+  filter() {
     console.log('filter');
   }
+
 
 }
