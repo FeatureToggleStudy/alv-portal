8f0aa3b8bd44bd99bbe87dc7909d818bea71a423
Patrick Dobler <patrick.dobler@gmail.com>
2019-01-10 14:27:03 +0100

    DF-484: fix ts-lint

--- alv-portal-ui/src/app/job-advertisement/shared/job-content/job-content.component.ts
+++ alv-portal-ui/src/app/job-advertisement/shared/job-content/job-content.component.ts
@@ -2,6 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { JobDetailModel } from '../job-detail-model';
 import { JobBadge } from '../job-badges-mapper.service';
 
+enum JobDetailPanelId {
+  JOB_AD_INFO = 'job-ad-info',
+  JOB_AD_REQUIREMENTS = 'job-ad-requirements',
+  JOB_AD_LANGUAGES = 'job-ad-languages',
+  JOB_AD_CONTACT_DETAILS = 'job-ad-contact-details'
+}
+
 @Component({
   selector: 'alv-job-content',
   templateUrl: './job-content.component.html',
@@ -23,11 +30,4 @@ export class JobContentComponent implements OnInit {
   ngOnInit() {
   }
 
-}
-
-enum JobDetailPanelId {
-  JOB_AD_INFO = 'job-ad-info',
-  JOB_AD_REQUIREMENTS = 'job-ad-requirements',
-  JOB_AD_LANGUAGES = 'job-ad-languages',
-  JOB_AD_CONTACT_DETAILS = 'job-ad-contact-details'
 }
