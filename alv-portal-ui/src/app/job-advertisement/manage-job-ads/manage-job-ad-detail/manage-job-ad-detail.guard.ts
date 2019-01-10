88a2554431355f547b0bc56487150e02213a2f24
Patrick Dobler <patrick.dobler@gmail.com>
2019-01-10 15:49:11 +0100

    DF-484: token aware guard

--- alv-portal-ui/src/app/job-advertisement/manage-job-ads/manage-job-ad-detail/manage-job-ad-detail.guard.ts
+++ alv-portal-ui/src/app/job-advertisement/manage-job-ads/manage-job-ad-detail/manage-job-ad-detail.guard.ts
@@ -7,28 +7,56 @@ import {
 import { ManageJobAdsState } from '../state-management/state';
 import { Store } from '@ngrx/store';
 import { JobAdvertisementRepository } from '../../../shared/backend-services/job-advertisement/job-advertisement.repository';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { JobAdvertisementDetailLoadedAction } from '../state-management/actions';
+import { AuthenticationService } from '../../../core/auth/authentication.service';
+import { hasAnyAuthorities, UserRole } from '../../../core/auth/user.model';
 
 @Injectable()
 export class ManageJobAdDetailGuard implements CanActivate {
 
-  constructor(private store: Store<ManageJobAdsState>, private jobAdvertisementRepository: JobAdvertisementRepository) {
+  constructor(private store: Store<ManageJobAdsState>,
+              private jobAdvertisementRepository: JobAdvertisementRepository,
+              private authenticationService: AuthenticationService) {
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     const id = next.params['id'];
-    return this.jobAdvertisementRepository.findById(id).pipe(
-      tap((jobAd) => {
-        this.store.dispatch(new JobAdvertisementDetailLoadedAction({ jobAdvertisement: jobAd }));
-      }),
-      map(() => {
-        return true;
-      }),
-      catchError(() => {
-        return of(false);
+    const token = next.queryParamMap.get('token');
+    if (token) {
+      return this.jobAdvertisementRepository.findByToken(token).pipe(
+        tap((jobAd) => {
+          this.store.dispatch(new JobAdvertisementDetailLoadedAction({ jobAdvertisement: jobAd }));
+        }),
+        map(() => {
+          return true;
+        }),
+        catchError(() => {
+          return of(false);
+        })
+      );
+    }
+    return this.authenticationService.getCurrentUser().pipe(
+      switchMap(value => {
+        const result = hasAnyAuthorities(value, [UserRole.ROLE_PAV, UserRole.ROLE_COMPANY]);
+        if (!result) {
+          return of(false);
+        }
+        return this.jobAdvertisementRepository.findById(id).pipe(
+          tap((jobAd) => {
+            this.store.dispatch(new JobAdvertisementDetailLoadedAction({ jobAdvertisement: jobAd }));
+          }),
+          map(() => {
+            return true;
+          }),
+          catchError(() => {
+            return of(false);
+          })
+        );
       })
     );
+
+
   }
 }
