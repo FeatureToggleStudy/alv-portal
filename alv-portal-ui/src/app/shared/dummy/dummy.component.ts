7365e0c49fa1c15b2519ef44ae5b95ccf0298ca6
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-21 14:03:45 +0100

    comment about dummy

--- alv-portal-ui/src/app/shared/dummy/dummy.component.ts
+++ alv-portal-ui/src/app/shared/dummy/dummy.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * This is a fake component that we need in order to provide it to the routes when no
+ * real component is needed there. For instance, if we want to assign a guard to a certain
+ * url path, but don't need any meaningful component attached to that path.
+ */
 @Component({
   selector: 'alv-dummy',
   template: ''
