591038f7f6b2cec51f2023b7886f3a0fd05a2d62
Sven Heinrich <sven.heinrich@mimacom.com>
2019-01-24 20:04:03 +0100

    Add job ad duplication id param

--- alv-portal-ui/src/app/job-publication/job-publication.resolver.ts
+++ alv-portal-ui/src/app/job-publication/job-publication.resolver.ts
@@ -1,28 +1,36 @@
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { flatMap, map, take } from 'rxjs/operators';
+import { flatMap, map, withLatestFrom } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { InitialFormValueConfig } from './job-publication-form/job-publication-form-value-factory';
 import { AuthenticationService } from '../core/auth/authentication.service';
+import { JobAdvertisementRepository } from '../shared/backend-services/job-advertisement/job-advertisement.repository';
 
 @Injectable()
 export class JobPublicationResolver implements Resolve<Observable<InitialFormValueConfig>> {
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService,
+              private jobAdvertisementRepository: JobAdvertisementRepository) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<InitialFormValueConfig> {
     const jobAdvertisementTitle = route.queryParams['job-title'];
-
-    //todo: load the job-advertisement that we want to copy
-    return this.authenticationService.getCurrentUser().pipe(
+    const duplicationJobAdvertisementId = route.queryParams['job-ad-id'];
+    const jobAdvertisement$ = duplicationJobAdvertisementId ? this.jobAdvertisementRepository.findById(duplicationJobAdvertisementId) : of(null);
+    const companyContactTemplate$ = this.authenticationService.getCurrentUser().pipe(
       flatMap((user) => (user ? this.authenticationService.getCurrentCompany() : of(null))),
-      map((companyContactTemplateModel) => ({ companyContactTemplateModel })),
-      map((initialFormValueConfig) => ({
-        ...initialFormValueConfig,
-        jobAdvertisementTitle
-      })),
-      take(1)
+      map((companyContactTemplateModel) => ({ companyContactTemplateModel })));
+
+    return jobAdvertisement$.pipe(
+      withLatestFrom(companyContactTemplate$),
+      map(([jobAdvertisement, initialFormValueConfig]) => {
+          return <InitialFormValueConfig>{
+            ...initialFormValueConfig,
+            jobAdvertisement,
+            jobAdvertisementTitle
+          };
+        }
+      )
     );
   }
 }
