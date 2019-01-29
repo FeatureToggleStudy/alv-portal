2426ec4aadc47f515cb27d6abba5f2aabd5e3bc7
emina-str <eminastricak@gmail.com>
2019-01-29 16:20:57 +0100

    DF 519 AGB Management - fixed comments from review

--- alv-portal-ui/src/app/admin/admin-routing.module.ts
+++ alv-portal-ui/src/app/admin/admin-routing.module.ts
@@ -2,15 +2,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { LegalTermsManagementComponent } from './legal-terms-management/legal-terms-management.component';
+import { UserRole } from '../core/auth/user.model';
 
 const routes: Routes = [
   {
     path: 'user-info',
     component: UserInfoComponent,
+    data: {
+      titleKey: 'portal.admin.user-info.user-info.title',
+      authorities: [UserRole.ROLE_SYSADMIN, UserRole.ROLE_ADMIN]
+    }
   },
   {
     path: 'legal-terms-management',
     component: LegalTermsManagementComponent,
+    data: {
+      titleKey: 'portal.admin.legal-terms-management.title',
+      authorities: [UserRole.ROLE_SYSADMIN, UserRole.ROLE_ADMIN]
+    }
   }
 ];
 
