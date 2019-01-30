0f48590fef6e4ca55b58ffd9a67226377169b77d
fago <fabian.gotzen@gmail.com>
2019-01-30 17:49:22 +0100

    DF-517 - display of system notification

--- alv-portal-ui/src/app/shared/layout/system-notification/system-notification.component.ts
+++ alv-portal-ui/src/app/shared/layout/system-notification/system-notification.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SystemNotificationDto } from '../../backend-services/system-notifications/system-notification.types';
+import {
+  SystemNotificationDto,
+  SystemNotificationType
+} from '../../backend-services/system-notifications/system-notification.types';
 import { I18nService } from '../../../core/i18n.service';
 
 @Component({
@@ -10,13 +13,34 @@ import { I18nService } from '../../../core/i18n.service';
 })
 export class SystemNotificationComponent implements OnInit {
 
-
   @Input() activeSystemNotifications: SystemNotificationDto[];
+
+  private visible = true;
 
   constructor(private i18nService: I18nService) {
   }
 
   ngOnInit() {
+  }
+
+  getTypeClass(type: SystemNotificationType): string {
+    return `system-notification-${type.toString().toLowerCase()}`;
+  }
+
+  getType(type: SystemNotificationType): string {
+    return `portal.admin.system-notifications.systemnotification.type.${type.toString().toLowerCase()}`;
+  }
+
+  getIconClass(type: SystemNotificationType): string {
+    return type.toString().toLowerCase() === SystemNotificationType.SYSTEMERROR ? `fa fa-ban` : `fa fa-exclamation`;
+  }
+
+  toggleVisible() {
+    this.visible = !this.visible;
+  }
+
+  getCurrentLanguage(type: SystemNotificationType) {
+    //todo fago translate the values
   }
 
 }
