10dc6eefa90c3e010c46bf7b350f3b602b07acde
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-31 11:21:14 +0100

    parsing hash fragment

--- alv-portal-ui/src/redirection.guard.ts
+++ alv-portal-ui/src/redirection.guard.ts
@@ -18,9 +18,11 @@ export class RedirectionGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    const match = state.url.match(/\/#\/job-publication-detail\/(.*)/);
-    const suffix = match[1];
-    if (match && suffix) {
+    const afterHashFragment: string = state.url.match(/\/#\/job-publication-detail\/(.*)/);
+    debugger;
+    const queryParamString = afterHashFragment.split('?');
+    const suffix = afterHashFragment[1];
+    if (afterHashFragment && suffix) {
       return this.router.navigate(['manage-job-ads', suffix]);
     }
     return false;
