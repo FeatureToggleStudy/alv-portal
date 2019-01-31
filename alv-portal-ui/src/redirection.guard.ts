9d5c606d25364c581b122220b0849727d6626261
Iurii Katkov <iurii.katkov@mimacom.com>
2019-01-31 16:13:43 +0100

    DF-506 redirection

--- alv-portal-ui/src/redirection.guard.ts
+++ alv-portal-ui/src/redirection.guard.ts
@@ -18,30 +18,14 @@ export class RedirectionGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    const afterHashFragment: RegExpMatchArray = state.url.match(/\/#\/job-publication-detail\/(.*)/);
-    const pageNameAndParams = afterHashFragment[1];
-    const pageNameAndParamsArr = pageNameAndParams.split('?');
-    const parsedParams = pageNameAndParamsArr[1];
-    debugger;
-    if (pageNameAndParamsArr.length > 2) {
-      // means that something went wrong
-      return this.router.navigate(['manage-job-ads']);
-    } else if (pageNameAndParamsArr.length === 1) {
-      //no query params
-      return this.router.navigate(['manage-job-ads', pageNameAndParams]);
-    } else {
-      // one or more correctly formatter query params
-      const queryParamsStr: string[] = parsedParams.split('&');
-      const queryParams = queryParamsStr.reduce((acc, paramString) => {
-        const keyValue = paramString.split('=');
-        const paramKey = keyValue[0];
-        const paramValue = keyValue[1];
-        const newPair = {};
-        newPair[paramKey] = paramValue;
-        return Object.assign(acc, newPair);
-        debugger;
-      }, {});
-      return this.router.navigate(['manage-job-ads', pageNameAndParamsArr[0]], { queryParams: queryParams});
+    const fragment = state.root.fragment;
+    if (!fragment) {
+      return true;
     }
+    const regExpMatchArray = fragment.match(/\/job-publication-detail\/(.*)/);
+    if (regExpMatchArray) {
+      return this.router.navigateByUrl('manage-job-ads/' + decodeURIComponent(regExpMatchArray[1]));
+    }
+    return true;
   }
 }
