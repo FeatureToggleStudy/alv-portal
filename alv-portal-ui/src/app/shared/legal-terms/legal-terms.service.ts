ef1036531bb8f7b8038a4955199852a85492c770
Marton Biro <marton.biro@mimacom.com>
2019-02-04 13:54:36 +0100

    DF-520: Change legal terms link on the registration page

---
+++ alv-portal-ui/src/app/shared/legal-terms/legal-terms.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { I18nService } from '../../core/i18n.service';
+import { combineLatest, Observable } from 'rxjs';
+import { LegalTermsManagementRepository } from '../backend-services/legal-terms-management/legal-terms-management-repository';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LegalTermsService {
+
+  constructor(private i18nService: I18nService,
+              private legalTermsManagementRepository: LegalTermsManagementRepository) {
+  }
+
+  getCurrentLegalTermsUrl(): Observable<string> {
+    const languageToLinkMap = {
+      en: 'linkEn',
+      de: 'linkDe',
+      fr: 'linkFr',
+      it: 'linkIt',
+    };
+
+    const currentLegalTerms$ = this.legalTermsManagementRepository.getCurrentLegalTerms();
+
+    return combineLatest(this.i18nService.currentLanguage$, currentLegalTerms$).pipe(
+      map(([currentLanguage, legalTerms]) => legalTerms[languageToLinkMap[currentLanguage]])
+    )
+  }
+}
