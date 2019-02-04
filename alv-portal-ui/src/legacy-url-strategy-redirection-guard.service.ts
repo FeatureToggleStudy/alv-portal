7c1e7648bd9dfbca60a970e5e0eb959aa48f8999
Iurii Katkov <iurii.katkov@mimacom.com>
2019-02-04 16:24:11 +0100

    DF-506 - unit tests

--- alv-portal-ui/src/legacy-url-strategy-redirection-guard.service.ts
+++ alv-portal-ui/src/legacy-url-strategy-redirection-guard.service.ts
@@ -16,7 +16,7 @@ interface LegacyUrlRedirection {
   providedIn: 'root'
 })
 /**
- * The guard need to redirect the user from the legacy urls (has-based strategy) to the
+ * The guard need to redirect the user from the legacy urls (hash-based strategy) to the
  * urls we use
  */
 export class LegacyUrlStrategyRedirectionGuard implements CanActivate {
@@ -24,23 +24,21 @@ export class LegacyUrlStrategyRedirectionGuard implements CanActivate {
   private legacyUrlRedirections: LegacyUrlRedirection[];
 
   constructor(private router: Router) {
-    this.legacyUrlRedirections = [
-      {
-        pattern: /^\/job-fingerprint-redirect\?(.*)/,
-        urlProvider: regExpMatchArray => {
-          return 'job-search/job-fingerprint-redirect?' + decodeURIComponent(regExpMatchArray[1]);
-        }
-      },
-      {
-        pattern: /^\/job-publication-detail\/(.*)/,
-        urlProvider: regExpMatchArray => {
-          return 'manage-job-ads/' + decodeURIComponent(regExpMatchArray[1]);
-        }
+    this.legacyUrlRedirections = [{
+      pattern: /^\/job-fingerprint-redirect\?(.*)/,
+      urlProvider: regExpMatchArray => {
+        return 'job-search/job-fingerprint-redirect?' + decodeURIComponent(regExpMatchArray[1]);
       }
+    }, {
+      pattern: /^\/job-publication-detail\/(.*)/,
+      urlProvider: regExpMatchArray => {
+        return 'manage-job-ads/' + decodeURIComponent(regExpMatchArray[1]);
+      }
+    }
     ];
   }
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): false {
     const fragment = state.root.fragment;
     if (!fragment) {
       return this.goHome();
@@ -64,7 +62,7 @@ export class LegacyUrlStrategyRedirectionGuard implements CanActivate {
     return false;
   }
 
-  goHome() {
+  private goHome(): false {
     this.router.navigate(['home']);
     return false;
   }
