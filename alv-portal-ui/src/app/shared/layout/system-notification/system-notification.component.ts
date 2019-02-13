c0843ded2e6071d13171a4791671e87060101895
Patrick Dobler <patrick.dobler@gmail.com>
2019-02-13 13:51:31 +0100

    fix review findings

--- alv-portal-ui/src/app/shared/layout/system-notification/system-notification.component.ts
+++ alv-portal-ui/src/app/shared/layout/system-notification/system-notification.component.ts
@@ -1,13 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {I18nService} from '../../../core/i18n.service';
-import {combineLatest, Observable} from 'rxjs';
-import {SystemNotificationRepository} from '../../backend-services/system-notifications/system-notification-repository';
-import {Notification, NotificationType} from '../notifications/notification.model';
+import { Component, OnInit } from '@angular/core';
+import { I18nService } from '../../../core/i18n.service';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { SystemNotificationRepository } from '../../backend-services/system-notifications/system-notification-repository';
+import { Notification, NotificationType } from '../notifications/notification.model';
 import {
   SystemNotificationDto,
   SystemNotificationType
 } from '../../backend-services/system-notifications/system-notification.types';
-import {map} from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
+import { AbstractSubscriber } from '../../../core/abstract-subscriber';
 
 function extractMessageKey(systemNotificationDto: SystemNotificationDto, currentLang: string) {
   switch (currentLang) {
@@ -32,37 +33,42 @@ function extractType(systemNotificationDto: SystemNotificationDto) {
   styleUrls: ['./system-notification.component.scss']
 
 })
-export class SystemNotificationComponent implements OnInit {
+export class SystemNotificationComponent extends AbstractSubscriber implements OnInit {
 
   NotificationType = NotificationType;
 
   currentLanguage$: Observable<string>;
 
-  systemNotifications$: Observable<Notification[]>;
+  systemNotifications$ = new BehaviorSubject<Notification[]>([]);
 
   constructor(private i18nService: I18nService,
               private systemNotificationRepository: SystemNotificationRepository) {
+    super();
   }
 
   ngOnInit() {
     this.currentLanguage$ = this.i18nService.currentLanguage$;
 
-    this.systemNotifications$ = combineLatest(this.i18nService.currentLanguage$, this.systemNotificationRepository.getActiveSystemNotifications())
-      .pipe(
-        map(([currentLang, systemNotifications]) => {
-          return systemNotifications.map(systemNotificationDto => {
-            return {
-              type: extractType(systemNotificationDto),
-              isSticky: true,
-              messageKey: extractMessageKey(systemNotificationDto, currentLang)
-            };
-          });
-        }));
+    combineLatest(this.i18nService.currentLanguage$, this.systemNotificationRepository.getActiveSystemNotifications()).pipe(
+      map(([currentLang, systemNotifications]) => {
+        return systemNotifications.map(systemNotificationDto => {
+          return {
+            type: extractType(systemNotificationDto),
+            messageKey: extractMessageKey(systemNotificationDto, currentLang),
+            isSticky: true
+          };
+        });
+      }),
+      takeUntil(this.ngUnsubscribe))
+      .subscribe(value => {
+        this.systemNotifications$.next(value);
+      });
   }
 
-
   hideSystemNotification(index: number) {
-    //FIXME pado this.systemNotifications$.
+    const notifications = this.systemNotifications$.value;
+    notifications.splice(index, 1);
+    this.systemNotifications$.next(notifications);
   }
 
 }
