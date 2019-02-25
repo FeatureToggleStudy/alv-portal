40f8a81932986bdb9195b980b9dfd0dbe76f62e3
Patrick Dobler <patrick.dobler@gmail.com>
2019-02-25 15:49:28 +0100

    Revert "DF-729: Sysadmin should be able to filter for restricted job ads"
    This reverts commit 07aa3d6381ec73ff185fa6db811df359284d92b2.

--- alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail.component.ts
+++ alv-portal-ui/src/app/candidate-search/candidate-detail/candidate-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Notification,
+  NotificationType
+} from '../../shared/layout/notifications/notification.model';
 import { CandidateDetailPanelId } from './candidate-detail-panel-id.enum';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
@@ -80,7 +84,7 @@ export class CandidateDetailComponent implements OnInit {
     this.canContactCandidatePerEmail$ = candidateProfile$.pipe(
       withLatestFrom(this.authenticationService.getCurrentUser()),
       map(([candidate, user]) => {
-        const rolePavOrCompany = hasAnyAuthorities(user, [UserRole.ROLE_PAV, UserRole.ROLE_COMPANY, UserRole.ROLE_SYSADMIN]);
+        const rolePavOrCompany = hasAnyAuthorities(user, [UserRole.ROLE_PAV, UserRole.ROLE_COMPANY]);
         const emailContactType = hasEmailContactType(candidate);
         return rolePavOrCompany && emailContactType;
       })
