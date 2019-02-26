b3857111ec3408fefdf718726725756b3bee14d2
Iurii Katkov <iurii.katkov@mimacom.com>
2019-02-26 13:31:03 +0100

    DF-634 - dashboard version works

--- alv-portal-ui/src/app/dashboard/dashboard-header/dashboard-header.component.ts
+++ alv-portal-ui/src/app/dashboard/dashboard-header/dashboard-header.component.ts
@@ -1,7 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../core/auth/user.model';
-import { CompanyContactTemplateModel } from '../../core/auth/company-contact-template-model';
 import { FormBuilder, FormControl } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import {
+  CoreState,
+  getAccountabilities,
+  getCurrentAccountability
+} from '../../core/state-management/state/core.state.ts';
+import { Observable } from 'rxjs';
+import { Accountability } from '../../shared/backend-services/user-info/user-info.types';
+import { SelectableOption } from '../../shared/forms/input/selectable-option.model';
+import { filter, map } from 'rxjs/operators';
+
+
+function mapAccountabilityToSelectOption(accountability) {
+  return {
+    label: accountability.companyName,
+    value: accountability.companyId
+  } as SelectableOption;
+}
 
 @Component({
   selector: 'alv-dashboard-header',
@@ -12,12 +29,25 @@ export class DashboardHeaderComponent {
 
   @Input() user: User;
 
-  @Input() company: CompanyContactTemplateModel;
+  currentAccountability: Observable<SelectableOption>;
+
+  accountabilities$: Observable<SelectableOption[]>;
 
   companyFormControl: FormControl;
 
-  constructor(private fb: FormBuilder) {
-    this.companyFormControl = null;
+  constructor(private fb: FormBuilder,
+              store: Store<CoreState>) {
+    this.companyFormControl = fb.control(null);
+    this.accountabilities$ = store.pipe(
+      select(getAccountabilities),
+      filter(Boolean),
+      map((accountabilities: Accountability[]) => accountabilities.map(mapAccountabilityToSelectOption))
+    );
+    this.currentAccountability = store.pipe(
+      select(getCurrentAccountability),
+      filter(Boolean),
+      map(mapAccountabilityToSelectOption)
+    ).subscribe(currentOption => this.companyFormControl.patchValue(currentOption.value));
   }
 
 }
