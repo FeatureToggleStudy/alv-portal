399102c8196eafaa638d47a5f83852e8b844092c
Iurii Katkov <iurii.katkov@mimacom.com>
2019-02-27 16:50:05 +0100

    DF-634 - not a nice way to dispatch accountability action on value chang

--- alv-portal-ui/src/app/shared/layout/accountability-switcher/accountability-switcher.component.ts
+++ alv-portal-ui/src/app/shared/layout/accountability-switcher/accountability-switcher.component.ts
@@ -5,13 +5,22 @@ import { FormBuilder, FormControl } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
 import {
   CoreState,
-  getAccountabilities, getCurrentAccountability
+  getAccountabilities,
+  getCurrentAccountability
 } from '../../../core/state-management/state/core.state.ts';
-import { filter, map } from 'rxjs/operators';
+import {
+  distinctUntilChanged,
+  filter,
+  map, share,
+  takeUntil,
+  withLatestFrom
+} from 'rxjs/operators';
 import { Accountability } from '../../backend-services/user-info/user-info.types';
+import { AbstractSubscriber } from '../../../core/abstract-subscriber';
+import { AccountabilitySelectedAction } from '../../../core/state-management/actions/core.actions';
 
 
-function mapAccountabilityToSelectOption(accountability) {
+function mapAccountabilityToSelectOption(accountability: Accountability) {
   return {
     label: accountability.companyName,
     value: accountability.companyId
@@ -23,14 +32,17 @@ function mapAccountabilityToSelectOption(accountability) {
   templateUrl: './accountability-switcher.component.html',
   styleUrls: ['./accountability-switcher.component.scss']
 })
-export class AccountabilitySwitcherComponent implements OnInit {
+export class AccountabilitySwitcherComponent extends AbstractSubscriber implements OnInit {
 
-  accountabilities$: Observable<SelectableOption[]>;
+  accountabilityOptions$: Observable<SelectableOption[]>;
   accontabilityFormControl: FormControl;
+
+  accountabilities$: Observable<Accountability[]>;
 
 
   constructor(private fb: FormBuilder,
               private store: Store<CoreState>) {
+    super();
   }
 
 
@@ -40,14 +52,31 @@ export class AccountabilitySwitcherComponent implements OnInit {
     this.accountabilities$ = this.store.pipe(
       select(getAccountabilities),
       filter(Boolean),
+      share());
+
+    this.accountabilityOptions$ = this.accountabilities$.pipe(
       map((accountabilities: Accountability[]) => accountabilities.map(mapAccountabilityToSelectOption))
     );
 
     this.store.pipe(
+      takeUntil(this.ngUnsubscribe),
       select(getCurrentAccountability),
       filter(Boolean),
       map(mapAccountabilityToSelectOption)
-    ).subscribe(currentOption => this.accontabilityFormControl.patchValue(currentOption.value));
+    ).subscribe(currentOption => {
+      this.accontabilityFormControl.patchValue(currentOption.value);
+    });
+
+    // this clumsiness indicated the need to refactor alv-select
+    this.accontabilityFormControl.valueChanges.pipe(
+      distinctUntilChanged(),
+      withLatestFrom(this.accountabilities$),
+      map(([selectedOption, accountabilities]: [SelectableOption, Accountability[]]) => {
+        this.store.dispatch(new AccountabilitySelectedAction({
+          accountability: accountabilities.find(a => a.companyId === selectedOption.value)
+        }));
+      })
+    );
   }
 
 }
