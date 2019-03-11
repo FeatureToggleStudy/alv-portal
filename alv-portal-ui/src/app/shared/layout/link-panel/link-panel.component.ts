afaeebb02b7e35c6062850b0a4157aa0bc9129a2
Iurii Katkov <iurii.katkov@mimacom.com>
2019-03-11 15:55:21 +0100

    DF-511 folder structure for link panels

--- alv-portal-ui/src/app/shared/layout/link-panel/link-panel.component.ts
+++ alv-portal-ui/src/app/shared/layout/link-panel/link-panel.component.ts
@@ -1,54 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-export enum LinkKind {
-  EXTERNAL = 'EXTERNAL',
-  INTERNAL = 'INTERNAL'
-}
-
-interface AbstractLink {
-
+export interface Link {
   text: string;
-
-  /**
-   * i18n key for a tooltip
-   */
-  tooltip?: string;
-
-}
-
-export interface InternalLink extends AbstractLink {
-  kind: LinkKind.INTERNAL;
-
-  routerLink: string[] | string;
-
-  queryParams?: { [index: string]: string };
-
-  fragment?: string | string[];
-}
-
-export interface ExternalLink extends AbstractLink {
-  kind: LinkKind.EXTERNAL;
-
   url: string;
 }
 
-
-export type LinkPanelItem = ExternalLink | InternalLink;
-
-const exampleExternal: LinkPanelItem = {
-  kind: LinkKind.EXTERNAL,
-  text: 'external stuff',
-  tooltip: 'external stuff tooltip',
-  url: 'http:///example.com'
-};
-
-const exampleInternal: LinkPanelItem = {
-  kind: LinkKind.INTERNAL,
-  text: 'internal stuff',
-  tooltip: 'internal tooltip',
-
-  fragment: ['one', 'two'],
-};
 
 @Component({
   selector: 'alv-link-panel',
@@ -58,9 +14,7 @@ const exampleInternal: LinkPanelItem = {
 export class LinkPanelComponent implements OnInit {
 
   @Input()
-  items: LinkPanelItem[];
-
-  LinkKind = LinkKind;
+  items: Link[];
 
   constructor() {
   }
