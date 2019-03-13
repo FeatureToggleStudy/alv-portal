e5cb565cd06e43b5fbdb302f26ee2da04220f8be
Iurii Katkov <iurii.katkov@mimacom.com>
2019-03-13 16:14:16 +0100

    DF-511 columns

--- alv-portal-ui/src/app/shared/layout/link-panel/link-panel.component.ts
+++ alv-portal-ui/src/app/shared/layout/link-panel/link-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LinkPanelData } from './links-repository';
 
+type GoodColumnNumber = 1 | 2;
+
 @Component({
   selector: 'alv-link-panel',
   templateUrl: './link-panel.component.html',
@@ -11,10 +13,23 @@ export class LinkPanelComponent implements OnInit {
   @Input()
   linkPanelData: LinkPanelData;
 
+  @Input()
+  numberOfColumns: GoodColumnNumber = 2;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+
+  range(lowEnd, highEnd): number[] {
+    const arr = [];
+    let c = highEnd - lowEnd + 1;
+    while (c--) {
+      arr[c] = highEnd--;
+    }
+    return arr;
   }
 
 }
