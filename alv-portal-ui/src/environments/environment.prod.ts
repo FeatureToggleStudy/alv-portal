994a105efe3ff0db6736eb8f17be96b7cadfc41e
Sven Heinrich <sven.heinrich@mimacom.com>
2019-03-20 16:33:57 +0100

    Remove the radius search feature toggle related code

--- alv-portal-ui/src/environments/environment.prod.ts
+++ alv-portal-ui/src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ export const environment = {
   production: true,
   translationBaseUrl: 'assets/i18n/',
   // replaced by the backend ch.admin.seco.alvportal.webapp.GoogleTrackingIdResourceTransformer
-  gaTrackingId: 'UA-XXXXXXXX-X',
-  jobAdRadiusSearchEnabled: false
+  gaTrackingId: 'UA-XXXXXXXX-X'
 };
