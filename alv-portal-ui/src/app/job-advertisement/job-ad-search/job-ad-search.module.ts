d4eab60b9fdb0b5685109511b85f3122641b4f9b
Patrick Dobler <patrick.dobler@gmail.com>
2019-04-08 16:56:14 +0200

    cleanup unused imports

--- alv-portal-ui/src/app/job-advertisement/job-ad-search/job-ad-search.module.ts
+++ alv-portal-ui/src/app/job-advertisement/job-ad-search/job-ad-search.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { JobSearchComponent } from './job-search/job-search.component';
-import { FilterPanelComponent } from './job-search/filter-panel/filter-panel.component';
-import { SharedModule } from '../../shared/shared.module';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { JobAdSearchRoutingModule } from './job-ad-search-routing.module';
-import { JobDetailComponent } from './job-detail/job-detail.component';
-import { StoreModule } from '@ngrx/store';
-import { JobAdSearchEffects, jobAdSearchReducer } from './state-management';
-import { EffectsModule } from '@ngrx/effects';
-import { JobSearchFilterParameterService } from './job-search/job-search-filter-parameter.service';
-import { JobDetailGuard } from './job-detail/job-detail.guard';
-import { JobSearchGuard } from './job-search/job-search.guard';
-import { WidgetsModule } from '../../widgets/widgets.module';
-import { SharedJobAdvertisementModule } from '../shared/shared-job-advertisement.module';
-import { JobFingerprintGuard } from './job-fingerprint-redirect/job-fingerprint.guard';
-import { ModalService } from '../../shared/layout/modal/modal.service';
-import { ComplaintModalComponent } from '../shared/complaint-modal/complaint-modal.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {JobSearchComponent} from './job-search/job-search.component';
+import {FilterPanelComponent} from './job-search/filter-panel/filter-panel.component';
+import {SharedModule} from '../../shared/shared.module';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import {JobAdSearchRoutingModule} from './job-ad-search-routing.module';
+import {JobDetailComponent} from './job-detail/job-detail.component';
+import {StoreModule} from '@ngrx/store';
+import {JobAdSearchEffects, jobAdSearchReducer} from './state-management';
+import {EffectsModule} from '@ngrx/effects';
+import {JobSearchFilterParameterService} from './job-search/job-search-filter-parameter.service';
+import {JobDetailGuard} from './job-detail/job-detail.guard';
+import {JobSearchGuard} from './job-search/job-search.guard';
+import {WidgetsModule} from '../../widgets/widgets.module';
+import {SharedJobAdvertisementModule} from '../shared/shared-job-advertisement.module';
+import {JobFingerprintGuard} from './job-fingerprint-redirect/job-fingerprint.guard';
+import {ModalService} from '../../shared/layout/modal/modal.service';
 
 @NgModule({
   imports: [
