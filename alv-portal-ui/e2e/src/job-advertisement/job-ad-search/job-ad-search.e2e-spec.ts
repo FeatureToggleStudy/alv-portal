31602688e38a7a48e6c68bfca71f79500ec50355
ganqqwerty <katkov.juriy@gmail.com>
2019-05-15 17:52:46 +0200

    e2e test for page reload of the previous and next buttons

--- alv-portal-ui/e2e/src/job-advertisement/job-ad-search/job-ad-search.e2e-spec.ts
+++ alv-portal-ui/e2e/src/job-advertisement/job-ad-search/job-ad-search.e2e-spec.ts
@@ -11,51 +11,51 @@ function getResultCount(page: JobSearchPo) {
 }
 
 describe('Job search page', () => {
-  let page: JobSearchPo;
+  let searchPage: JobSearchPo;
 
   beforeEach(() => {
-    page = new JobSearchPo();
-    page.navigateTo();
+    searchPage = new JobSearchPo();
+    searchPage.navigateTo();
   });
 
-  it('should navigate to the job-search page', () => {
-    expect(page.browserTitle).toEqual('Search job');
+  it('should navigate to the job-search searchPage', () => {
+    expect(searchPage.browserTitle).toEqual('Search job');
   });
 
   it('should have some more than 0 results with empty search query', () => {
-    expect(page.filterPanel.isResultCountPresent).toBe(true, 'the result counter is not rendered at all');
-    expect(page.filterPanel.resultCount).toBeGreaterThan(0, 'the result counter is more than 0'); // parseInt will cut everything after . character, so if in English locale big number are written like this '85.111' instead of this '85'111', the result won't be precise. We don't care about that here though.
-    expect(page.searchResultsPanel.resultCount).toBe(PAGE_SIZE);
+    expect(searchPage.filterPanel.isResultCountPresent).toBe(true, 'the result counter is not rendered at all');
+    expect(searchPage.filterPanel.resultCount).toBeGreaterThan(0, 'the result counter is more than 0'); // parseInt will cut everything after . character, so if in English locale big number are written like this '85.111' instead of this '85'111', the result won't be precise. We don't care about that here though.
+    expect(searchPage.searchResultsPanel.resultCount).toBe(PAGE_SIZE);
   });
 
   it('should have working pagination', () => {
-    page.scrollToBottom()
+    searchPage.scrollToBottom()
       .then(() => {
-        expect(page.searchResultsPanel.resultCount).toBe(PAGE_SIZE * 2, 'the amount of results fetched after pagination is not two pages');
+        expect(searchPage.searchResultsPanel.resultCount).toBe(PAGE_SIZE * 2, 'the amount of results fetched after pagination is not two pages');
       });
   });
 
   it('should decrease the amount of found jobs when the filters is applied', async () => {
-    const initialCount = await getResultCount(page);
-    await page.filterPanel.setContractType(ContractTypes.TEMPORARY);
-    const filteredByContractTypeCount = await getResultCount(page);
+    const initialCount = await getResultCount(searchPage);
+    await searchPage.filterPanel.setContractType(ContractTypes.TEMPORARY);
+    const filteredByContractTypeCount = await getResultCount(searchPage);
     expect(filteredByContractTypeCount).toBeLessThan(initialCount);
-    await page.filterPanel.setCompany('GmbH');
-    const filteredByCompanyCount = await getResultCount(page);
+    await searchPage.filterPanel.setCompany('GmbH');
+    const filteredByCompanyCount = await getResultCount(searchPage);
     expect(filteredByCompanyCount).toBeLessThan(filteredByContractTypeCount);
   });
 
   it('should perform search by the criteria in the upper panel', async () => {
-    const initialCount = await getResultCount(page);
-    await page.mainFilterPanel.fillOccupation('pro');
+    const initialCount = await getResultCount(searchPage);
+    await searchPage.mainFilterPanel.fillOccupation('pro');
     await browser.sleep(DEFAULT_SLEEP_TIME);
-    const afterFilter = await getResultCount(page);
+    const afterFilter = await getResultCount(searchPage);
     expect(afterFilter).toBeLessThan(initialCount);
   });
 
-  it('should go to a details page and navigate to next search results', async () => {
-    const resultsHeaders = await page.searchResultsPanel.getResultHeaders();
-    await page.searchResultsPanel.clickOnResult(0);
+  it('should go to a details searchPage and navigate to next search results', async () => {
+    const resultsHeaders = await searchPage.searchResultsPanel.getResultHeaders();
+    await searchPage.searchResultsPanel.clickOnResult(0);
     const detailsPage = new JobDetailsPo();
 
     //check that all headers match the search results headers
@@ -65,21 +65,21 @@ describe('Job search page', () => {
     }
     //we've reached the last result header and the new ones have to be loaded now. The next button must be clickable
 
-    await expect(detailsPage.nextButton.isEnabled()).toBe(true, 'The next button must be clickable after iterating through the first page of results');
+    await expect(detailsPage.nextButton.isEnabled()).toBe(true, 'The next button must be clickable after iterating through the first searchPage of results');
 
     await detailsPage.goBackToSearch();
-    await expect(page.searchResultsPanel.results.$$('.visited').count()).toBe(21, 'must be 21 visited search results');
+    await expect(searchPage.searchResultsPanel.results.$$('.visited').count()).toBe(21, 'must be 21 visited search results');
 
   });
 
   it('should navigate from the last to the first search result normally', async () => {
-    await page.scrollToBottom();
-    const resultHeaders = await page.searchResultsPanel.getResultHeaders();
+    await searchPage.scrollToBottom();
+    const resultHeaders = await searchPage.searchResultsPanel.getResultHeaders();
     await expect(resultHeaders.length).toBe(40);
 
-    await page.scrollToBottom();
+    await searchPage.scrollToBottom();
     //now let's go back from the 40th result to a 0th result
-    await page.searchResultsPanel.clickOnResult(39);
+    await searchPage.searchResultsPanel.clickOnResult(39);
 
     const detailsPage = new JobDetailsPo();
 
@@ -91,8 +91,18 @@ describe('Job search page', () => {
     await expect(detailsPage.previousButton.isEnabled()).toBeFalsy();
 
     await detailsPage.goBackToSearch();
-    await expect(page.searchResultsPanel.results.$$('.visited').count()).toBe(40);
+    await expect(searchPage.searchResultsPanel.results.$$('.visited').count()).toBe(40);
   });
+
+  it('should disable previous and next buttons if we reload the details searchPage', async () => {
+    await searchPage.searchResultsPanel.clickOnResult(3);
+    const detailsPage = new JobDetailsPo();
+    expect(detailsPage.previousButton.isEnabled()).toBe(true);
+    expect(detailsPage.nextButton.isEnabled()).toBe(true);
+    browser.refresh();
+    expect(detailsPage.previousButton.isEnabled()).toBe(false);
+    expect(detailsPage.nextButton.isEnabled()).toBe(false);
+  })
 
 });
 
