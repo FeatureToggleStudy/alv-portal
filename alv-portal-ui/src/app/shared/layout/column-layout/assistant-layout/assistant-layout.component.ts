e902d5a83bf96d2acf83ee30a8796815a103d07c
ganqqwerty <katkov.juriy@gmail.com>
2019-06-17 10:39:23 +0200

    DF-986 added a todo

--- alv-portal-ui/src/app/shared/layout/column-layout/assistant-layout/assistant-layout.component.ts
+++ alv-portal-ui/src/app/shared/layout/column-layout/assistant-layout/assistant-layout.component.ts
@@ -5,6 +5,8 @@ import { AbstractColumnLayout } from '../abstract-column-layout';
  * It's one of the kinds of three-column layout (2:7:3) for using with the assistants.
  * In the assistants the left panel has enough space to fit the nivigation links
  * and the right panels is big enough to fit the additional information
+ *
+ * todo maybe merge it with three-column layout and parametrize it
  */
 @Component({
   selector: 'alv-assistant-layout',
