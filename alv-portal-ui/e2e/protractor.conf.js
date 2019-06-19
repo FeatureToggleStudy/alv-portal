e5d8caf53cd1cabd197cc0000d65de4602e75f73
ganqqwerty <katkov.juriy@gmail.com>
2019-06-19 14:10:30 +0200

    DF-986 e2e fix

--- alv-portal-ui/e2e/protractor.conf.js
+++ alv-portal-ui/e2e/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--window-size=1024,768" ]
+      args: ["--headless", "--disable-gpu", "--window-size=1280,960"]
     }
   },
   directConnect: true,
