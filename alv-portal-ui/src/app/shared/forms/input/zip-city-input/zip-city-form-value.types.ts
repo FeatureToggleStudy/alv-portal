5c8d4f60b134f2c2ac24c8a0573adc9171b63f83
ganqqwerty <katkov.juriy@gmail.com>
2019-06-20 16:12:02 +0200

    DF-986 stronger types

--- alv-portal-ui/src/app/shared/forms/input/zip-city-input/zip-city-form-value.types.ts
+++ alv-portal-ui/src/app/shared/forms/input/zip-city-input/zip-city-form-value.types.ts
@@ -2,16 +2,14 @@ import { ZipAndCityTypeaheadItem } from '../../../localities/zip-and-city-typeah
 import { ValidatorFn } from '@angular/forms';
 
 export interface ZipCityFormValue {
-  zipCityAutoComplete?: ZipAndCityTypeaheadItem;
-  zipCode?: string;
-  city?: string;
+  zipCityAutoComplete: ZipAndCityTypeaheadItem;
+  zipCode: string;
+  city: string;
 }
 
 type Index = keyof ZipCityFormValue;
 
-//todo this type is not strong enough, but helps with autocompletion
 export type ZipCityValidators = { [i in Index]: ValidatorFn[] }
-
 
 export function emptyZipCityFormValue() {
   return {
