031cfecb8c8fa332e879a00fcffe5a93d2c2f60d
Marton Biro <marton.biro@mimacom.com>
2019-07-11 17:26:45 +0200

    Revert "Update CandidateContactRepository URL according to the backend changes"
    This reverts commit 2a382ddf

--- alv-portal-ui/src/app/shared/backend-services/candidate/candidate-contact-repository.ts
+++ alv-portal-ui/src/app/shared/backend-services/candidate/candidate-contact-repository.ts
@@ -6,13 +6,11 @@ import { EmailContactModal } from './candidate.types';
 @Injectable({ providedIn: 'root' })
 export class CandidateContactRepository {
 
-  private resourceUrl = '/candidateservice/api';
-
   constructor(private httpClient: HttpClient) {
   }
 
   sendContactModalEmail(emailContactModal: EmailContactModal): Observable<void> {
-    return this.httpClient.post<void>(`${this.resourceUrl}/messages/send-anonymous-message`, emailContactModal);
+    return this.httpClient.post<void>('/api/messages/send-anonymous-message', emailContactModal);
   }
 
 }
