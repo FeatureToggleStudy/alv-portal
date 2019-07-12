3f5487b7e194c22723c1b4ac20ef823a1b05010d
ganqqwerty <katkov.juriy@gmail.com>
2019-07-12 14:06:12 +0200

    removed the animation to maker it work with ie

--- alv-portal-ui/src/app/shared/layout/collapse-panel/collapse-panel.component.ts
+++ alv-portal-ui/src/app/shared/layout/collapse-panel/collapse-panel.component.ts
@@ -8,17 +8,18 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('expandCollapse', [
       state('open', style({
+        display: 'block !important' ,
         'max-height': '100%',
         transform: 'scaleY(1)',
         opacity: 1
       })),
       state('closed', style({
-        height: '1px',
-        'max-height': '1px',
+        display: 'none',
+        height: 0,
+        'max-height': 0,
         transform: 'scaleY(0)',
         opacity: 0
       })),
-      transition('* => *', [animate('200ms')])
     ])
   ]
 })
