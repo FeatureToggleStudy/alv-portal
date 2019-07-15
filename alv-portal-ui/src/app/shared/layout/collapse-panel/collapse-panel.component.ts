9ec85b1e70849759e1465d6ac44ab17d39488d1a
ganqqwerty <katkov.juriy@gmail.com>
2019-07-15 13:16:06 +0200

    hack fix of collapse panel. Tested on all browsers

--- alv-portal-ui/src/app/shared/layout/collapse-panel/collapse-panel.component.ts
+++ alv-portal-ui/src/app/shared/layout/collapse-panel/collapse-panel.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-
+// Angular animations have problems with animating the transition between display:none and display:block, so we couldn't
+// implement the animation the simple way. Instead, the following hack was used:
+// 1. We only collapse to 1 px, so that no optimizer can remove the panel content from the DOM
+// 2. We use overflow:hidden on a .panel-content container so that when it shrinks, the elements inside are not shown - in css
+// 3.We use min-height: 0 on a .panel-content container to allow schrinking on IE - in css
+// 3. the rest is done in animations below. We purposefully remove paddings on collapsed, because otherwise the top of the first
+// element will be visible inside the closed panel.
+// If you think that you find simpler way to collapsing, please test it thoroughly with IE and Safari.
 @Component({
   selector: 'alv-collapse-panel',
   templateUrl: './collapse-panel.component.html',
@@ -8,18 +15,20 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('expandCollapse', [
       state('open', style({
-        display: 'block !important' ,
         'max-height': '100%',
-        transform: 'scaleY(1)',
-        opacity: 1
+        visibility: 'visible',
+        'padding-top': '0.5em',
+        'padding-bottom': '0.5em'
       })),
       state('closed', style({
-        display: 'none',
-        height: 0,
-        'max-height': 0,
-        transform: 'scaleY(0)',
-        opacity: 0
+        height: '9px',
+        'max-height': '1px',
+        visibility: 'hidden',
+        'padding-top': '0',
+        'padding-bottom': '0'
+
       })),
+      transition('* => *', [animate('200ms')])
     ])
   ]
 })
