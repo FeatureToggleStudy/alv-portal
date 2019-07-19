c987adfad82d2674e0cbb477d75c7ee26dad10d3
ganqqwerty <katkov.juriy@gmail.com>
2019-07-19 17:49:40 +0200

    DF-1117 no -> cancel

--- alv-portal-ui/src/app/online-services/work-efforts/work-efforts-overview/modal-config.types.ts
+++ alv-portal-ui/src/app/online-services/work-efforts/work-efforts-overview/modal-config.types.ts
@@ -1,6 +1,9 @@
-export const deleteWorkEffortModalConfig = {
+import { ConfirmModalConfig } from '../../../shared/layout/modal/confirm-modal/confirm-modal-config.model';
+
+export const deleteWorkEffortModalConfig: ConfirmModalConfig = {
   title: 'portal.work-efforts.work-effort.confirm-delete-modal.title',
   content: 'portal.work-efforts.work-effort.confirm-delete-modal.question',
-  confirmLabel: 'portal.work-efforts.work-effort.confirm-delete-modal.confirm.label'
+  confirmLabel: 'portal.work-efforts.work-effort.confirm-delete-modal.confirm.label',
+  cancelLabel: 'portal.work-efforts.work-effort.confirm-discard-modal'
 };
 
