cb89c25eff4be50a4fa3ddcea675d9248ea7f158
ganqqwerty <katkov.juriy@gmail.com>
2019-07-22 11:51:41 +0200

    DF-1176 removed unnecessary translations, fixed translations

--- alv-portal-ui/src/app/online-services/work-efforts/work-efforts-overview/modal-config.types.ts
+++ alv-portal-ui/src/app/online-services/work-efforts/work-efforts-overview/modal-config.types.ts
@@ -4,6 +4,5 @@ export const deleteWorkEffortModalConfig: ConfirmModalConfig = {
   title: 'portal.work-efforts.work-effort.confirm-delete-modal.title',
   content: 'portal.work-efforts.work-effort.confirm-delete-modal.question',
   confirmLabel: 'portal.work-efforts.work-effort.confirm-delete-modal.confirm.label',
-  cancelLabel: 'portal.work-efforts.work-effort.confirm-discard-modal'
 };
 
