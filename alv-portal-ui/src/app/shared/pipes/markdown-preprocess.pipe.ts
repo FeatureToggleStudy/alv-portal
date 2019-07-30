dba5f745f09dab2608f8d902d3eeb19ce7be2582
Sven Heinrich <sven.heinrich@mimacom.com>
2019-07-30 22:03:39 +0200

    Add tests for markdownPreprocessPipe

--- alv-portal-ui/src/app/shared/pipes/markdown-preprocess.pipe.ts
+++ alv-portal-ui/src/app/shared/pipes/markdown-preprocess.pipe.ts
@@ -1,8 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 /**
- * Inspired by:
- * https://github.com/opichals/remove-markdown
+ * This pipe transforms certain non-markdown tokens (like the bullet point for a list)
+ * to known markdown tokens (like the dash for a list).
+ * Currently transformed:
+ * - Bullet points (all 4 sizes) -> dashes
  */
 @Pipe({
   name: 'markdownPreprocess'
@@ -11,11 +13,22 @@ export class MarkdownPreprocessPipe implements PipeTransform {
 
   transform(value: any): any {
     if (value) {
-      return value.replace(/·/gm, '- ')
-        .replace(/∙/gm, '- ')
-        .replace(/●/gm, '- ');
+      return this.replaceBulletPoints(value);
     }
     return value;
   }
 
+  private replaceBulletPoints(value: string): string {
+    return value.replace(new RegExp(BulletPoints.SMALL_BULLET, 'gm'), '- ')
+      .replace( new RegExp(BulletPoints.MEDIUM_BULLET, 'gm'), '- ')
+      .replace(new RegExp(BulletPoints.OPERATOR_BULLET, 'gm'), '- ')
+      .replace(new RegExp(BulletPoints.LARGE_BULLET, 'gm'), '- ');
+  }
+}
+
+export enum BulletPoints {
+  SMALL_BULLET = String.fromCharCode(183),
+  MEDIUM_BULLET = String.fromCharCode(8226),
+  OPERATOR_BULLET = String.fromCharCode(8729),
+  LARGE_BULLET = String.fromCharCode(9679)
 }
