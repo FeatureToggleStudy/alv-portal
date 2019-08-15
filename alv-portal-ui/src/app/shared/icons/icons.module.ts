44c5d3afe345b059eab4793799afb7d7a7a351b5
Sven Heinrich <sven.heinrich@mimacom.com>
2019-08-15 21:58:34 +0200

    Add pro SVG icons library, use pro icon

--- alv-portal-ui/src/app/shared/icons/icons.module.ts
+++ alv-portal-ui/src/app/shared/icons/icons.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons';
 import { SvgFixingDirective } from './svg-fixing.directive';
 import { CustomIconComponent } from './custom-icon/custom-icon.component';
 
-library.add(fas);
 library.add(far);
+library.add(fas);
+library.add(fasPro);
 
 @NgModule({
   declarations: [
