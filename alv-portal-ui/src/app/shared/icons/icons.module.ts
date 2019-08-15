fe7050d260443d973651f9ee132606c763e2707e
Sven Heinrich <sven.heinrich@mimacom.com>
2019-08-15 22:19:16 +0200

    Migrate to angular-fontawesome 0.5

--- alv-portal-ui/src/app/shared/icons/icons.module.ts
+++ alv-portal-ui/src/app/shared/icons/icons.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons';
 import { SvgFixingDirective } from './svg-fixing.directive';
 import { CustomIconComponent } from './custom-icon/custom-icon.component';
-
-library.add(far);
-library.add(fas);
-library.add(fasPro);
 
 @NgModule({
   declarations: [
@@ -29,4 +24,9 @@ library.add(fasPro);
   ]
 })
 export class IconsModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(far);
+    library.addIconPacks(fas);
+    library.addIconPacks(fasPro);
+  }
 }
